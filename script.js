014cb80c44b27ed4a48e8bc18f63fcbc703eae88
Jose-Jpan <jose.santos.soares.nz@gmail.com>
2024-08-18 12:03:04 +0900

    For loop implemented

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let age = 30;
 
 console.log(window.name); // Carlos Santana
 console.log(window.age);  // undefined
+
+for (var i = 1; i <= 10; i++) {
+    console.log(i); // 1, 2, 3, 4... 10
+}
+
+console.log(i); // Will print the last value of i: 10
